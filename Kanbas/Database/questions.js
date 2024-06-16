cc03c5adee4ab5db6b32684583b5672ec2778e87
iris19951229 <shaonian1229@gmail.com>
2024-06-15 21:24:10 -0700

    push for deploying first

---
+++ Kanbas/Database/questions.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        "_id": "quiz1",
+        "title" : "Question 1",
+        "question_type": "Multiple Choice",
+        "descriptiont": "Which of the following is the primary advantage of a liquid rocket engine over a solid rocket engine?",
+        "options": [
+          "Higher specific impulse",
+          "Lower cost",
+          "Simpler design",
+          "Shorter burn time"
+        ],
+        "correctChoiceIndex": 1,
+        "points": 1,
+        
+      },
+      {
+        "_id": "quiz1",
+        "title" : "Question 2",
+        "question_type": "True/False",
+        "description": "Specific impulse is a measure of how efficiently a rocket uses its fuel.",
+        "correctChoiceIndex": 1,
+        "points": 1,
+    
+      },
+      {
+        "_id": "quiz1",
+        "title": "Question 3",
+        "question_type": "Fill in the Blanks",
+        "description": "The ___a_____ effect and the ___b_____ principle are fundamental to rocket propulsion.",
+        "blanks": [
+        { "label": "a", "answer": "Newton's Third Law" },
+        { "label": "b", "answer": "conservation of momentum" }
+         ],
+        "points": 1,
+     
+      },
+    
+      
+    ]
