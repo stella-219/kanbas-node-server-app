e782b3bd0a9a596442a226cd84128317d2f09f0b
iris19951229 <shaonian1229@gmail.com>
2024-06-25 20:23:59 -0700

    latest version on 6/25/2024

--- Users/schema.js
+++ Users/schema.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     role: {
       type: String,
       enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
-      default: "USER",
+      default: "STUDENT",
     },
     loginId: String,
     section: String,
